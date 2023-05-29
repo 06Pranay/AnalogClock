abb8a78ca2e1cbb36c72c9239d196b8af5fce5ce
Pranay <06pranay@gmail.com>
2023-05-29 19:53:45 +0530

    Analogwatch

---
+++ index.js
@@ -0,0 +1,15 @@
+setInterval(()=>
+{
+    date=new Date();
+    hourtime=date.getHours();
+    minutetime=date.getMinutes();
+    secondtime=date.getSeconds();
+    hourrotation=30*hourtime+minutetime/2;
+    minuterotation=6*minutetime;
+    secondrotation=6*secondtime;
+
+    hour.style.transform=`rotate(${hourrotation}deg)`;
+    minute.style.transform=`rotate(${minuterotation}deg)`;
+    second.style.transform=`rotate(${secondrotation}deg)`;
+
+},1000);
